46156e957962488b3fd58dd9a4d0aa30a9c64d7e
Sandra Gurgzdaityte <sandragurgzdaityte@Sandras-MacBook-Air.local>
2022-08-08 17:25:02 +0100

    String concatenation

--- madlib.py
+++ madlib.py
@@ -7,3 +7,5 @@ madlib = f"Computer programming is so {adj}! It makes me so excited all the time
  f"I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
+
+# commit
